86cc90bcb4941efa6eee39aecbefaf9383da42f0
ysf-RAMI <youssef.rami.08@edu.uiz.ac.ma>
2025-03-05 16:57:25 +0000

    Remove unused AdsClick import from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { AdsClick } from "@mui/icons-material";
 
 export default defineConfig({
   plugins: [react()],
